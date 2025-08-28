c3797def9eb080b05c600e0a109359ddfdb70a3a
sonu536 <35693329+sonu536@users.noreply.github.com>
2025-08-28 22:34:52 +0530

    Update locker.js

--- locker.js
+++ locker.js
@@ -1,20 +1,43 @@
-$(function() {
-  // get the <script> tag that loaded this file
-  const thisScript = document.currentScript;
-  const urlObj = new URL(thisScript.src);
+$(function () {
+  // Find the script tag that loaded locker.js
+  const scripts = document.getElementsByTagName("script");
+  let thisScript = null;
 
-  // read aff_sub4 from the script's query string
-  const aff_sub4 = urlObj.searchParams.get("aff_sub4");
+  for (let s of scripts) {
+    if (s.src.includes("locker.js")) {
+      thisScript = s;
+      break;
+    }
+  }
 
-  // now use it in your request
-  var url = 'https://ogads.vercel.app' + '?aff_sub4=' + aff_sub4;
+  let aff_sub4 = null;
+  if (thisScript) {
+    const urlObj = new URL(thisScript.src);
+    aff_sub4 = urlObj.searchParams.get("aff_sub4");
+  }
 
-  $.getJSON(url, null, function(offers){
-    var html = '';
-    var numOffers = 5; //Change to trim offers. Max is 10.
-    offers = offers.splice(0,numOffers);
-    $.each(offers, function(key, offer){
-      html += '<center><div id="offer"><a class="offer" href="'+offer.link+'" target="_blank">'+offer.name_short+'<p>'+offer.adcopy+'</p></a></div></center>';
+  // Fallback: also check page URL if not found
+  if (!aff_sub4) {
+    const pageUrl = new URL(window.location.href);
+    aff_sub4 = pageUrl.searchParams.get("aff_sub4");
+  }
+
+  // Build request URL
+  const url = "https://ogads.vercel.app?aff_sub4=" + encodeURIComponent(aff_sub4 || "");
+
+  $.getJSON(url, null, function (offers) {
+    var html = "";
+    var numOffers = 5; // Change to trim offers. Max is 10.
+    offers = offers.splice(0, numOffers);
+    $.each(offers, function (key, offer) {
+      html +=
+        '<center><div id="offer"><a class="offer" href="' +
+        offer.link +
+        '" target="_blank">' +
+        offer.name_short +
+        "<p>" +
+        offer.adcopy +
+        "</p></a></div></center>";
     });
     $("#offerContainer").append(html);
   });
