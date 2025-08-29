e93a9d6b0cb663dc1d7c37ab7712670a9c42f138
sonu536 <35693329+sonu536@users.noreply.github.com>
2025-08-29 13:02:12 +0530

    Update locker.js

--- locker.js
+++ locker.js
@@ -1,16 +1,21 @@
 $(function() {
-  let url_string = window.location.href;
-  let url1 = new URL(url_string);
-  let aff_sub4 = url1.searchParams.get('aff_sub4');
+  // get the <script> tag that loaded this file
+  const thisScript = document.currentScript;
+  const urlObj = new URL(thisScript.src);
+
+  // read aff_sub4 from the script's query string
+  const aff_sub4 = urlObj.searchParams.get("aff_sub4");
+
+  // now use it in your request
   var url = 'https://ogads.vercel.app' + '?aff_sub4=' + aff_sub4;
-  $.getJSON(url, null,
-    function(offers){
-      var html = '';
-      var numOffers=5; //Change to trim offers. Max is 10.
-      offers=offers.splice(0,numOffers);
-      $.each(offers, function(key, offer){
-        html += '<center><div id="offer"><a class="offer" href="'+offer.link+'" target="_blank">'+offer.name_short+'<p>'+offer.adcopy+'</p></a></div></center>';
-      });
-      $("#offerContainer").append(html);
+
+  $.getJSON(url, null, function(offers){
+    var html = '';
+    var numOffers = 5; //Change to trim offers. Max is 10.
+    offers = offers.splice(0,numOffers);
+    $.each(offers, function(key, offer){
+      html += '<center><div id="offer"><a class="offer" href="'+offer.link+'" target="_blank">'+offer.name_short+'<p>'+offer.adcopy+'</p></a></div></center>';
     });
+    $("#offerContainer").append(html);
+  });
 });
