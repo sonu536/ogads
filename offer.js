5e5ea5bdd6b35037a2dc8ef19b57d8a88b3a2d10
sonu536 <35693329+sonu536@users.noreply.github.com>
2025-10-02 19:44:53 +0530

    Create offer.js

---
+++ offer.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const payoutGroups = [
+    { countries: ["US", "CA", "FR", "AU", "DE", "GB", "NL", "IT"], minPayout: 0.45 },
+    { countries: ["MY","TR","ZA","PL","NO","JP","HK"], minPayout: 0.20 }
+  ];
+  const defaultMinPayout = 0.07;
+  let urlString = window.location.href;
+  let url1 = new URL(urlString);
+  let aff_sub4 = url1.searchParams.get('aff_sub4');
+  let url = 'https://ogads.vercel.app' + '?aff_sub4=' + aff_sub4;
+  fetch(url)
+    .then(response => response.json())
+    .then(offers => {
+      offers = offers.filter(function (offer) {
+        let offerCountries = offer.country.split(',').map(c => c.trim());
+        let payout = parseFloat(offer.payout);
+        let inAnyGroup = false;
+        let passesGroups = payoutGroups.every(function (group) {
+          const inGroup = group.countries.some(c => offerCountries.includes(c));
+          if (inGroup) {
+            inAnyGroup = true;
+            return payout >= group.minPayout;
+          }
+          return true;
+        });
+        if (!inAnyGroup) {
+          return payout >= defaultMinPayout;
+        }
+        return passesGroups;
+      });
+      let html = '';
+      let numOffers = 5; // Limit max offers
+      offers = offers.slice(0, numOffers);
+
+      offers.forEach(function (offer) {
+        html += '<center><div id="offer">'+'<a class="offer" href="'+ offer.link +'" target="_blank">'+offer.name_short+'<p>'+ offer.adcopy+'</p>'+'</a>'+'</div></center>';
+      });
+      document.querySelector("#offerContainer").insertAdjacentHTML("beforeend", html);
+    })
+});
